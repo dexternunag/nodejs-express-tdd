6eabcba7430dd2712252ea1c2201983fbed6ecb9
Dexter <nunagdexter@gmail.com>
2021-08-09 13:58:01 +0800

    Created app test file for api todos api

--- app.test.js
+++ app.test.js
@@ -1,6 +1,63 @@
 const request = require('supertest')
 const app = require('./app')
 
-it('should run', () => {
+describe('Todos API', () => {
+    it('GET /todos --> array todos', () => {
+        return request(app)
+                .get('/todos')
+                .expect('Content-Type', /json/)
+                .expect(200)
+                .then((res) => {
+                    expect(res.body).toEqual(expect.arrayContaining([
+                        expect.objectContaining({
+                            name: expect.any(String),
+                            completed: expect.any(Boolean)
+                        })
+                    ]))
+                })
+    })
 
+    it('GET /todos/:id --> specific todo by ID', () => {
+        return request(app)
+                .get('/todos/1')
+                .expect('Content-Type', /json/)
+                .expect(200)
+                .then((res) => {
+                    expect(res.body).toEqual(expect.objectContaining({
+                        name: expect.any(String),
+                        completed: expect.any(Boolean)
+                    }))
+                })
+    })
+
+    it('GET /todos/:id --> 404 if not found', () => {
+        return request(app)
+                .get('/todos/123')
+                .expect(404)
+    })
+
+    it('POST /todos --> created todo', () => {
+        return request(app)
+                .post('/todos')
+                .send({
+                    name: 'Do testing'
+                })
+                .expect('Content-Type', /json/)
+                .expect(201)
+                .then((res) => {
+                    expect(res.body).toEqual(expect.objectContaining({
+                        name: expect.any('Do testing'),
+                        completed: false
+                    }))
+                })
+    })
+
+    it('POST /todos --> validates request todo body', () => {
+        return request(app)
+                .post('/todos')
+                .send({
+                    name: 123
+                })
+                .expect(422)
+    })
 })
